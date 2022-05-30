cbe2d7754b23e2bfbcc57f05e639b07b20d64784
TRETomas <99128577+TRETomas@users.noreply.github.com>
2022-05-30 12:55:10 +0200

    modif modele.php
    ajout de fonctions + une à modif

--- libs/modele.php
+++ libs/modele.php
@@ -15,21 +15,18 @@ function genererJson ($view, $recherche=" * ", $idUser)
 function mkPlaylist ($nom_playlist, $lien_image="ressources/default.png", $tabMus)
 {
     // Cette fonction crée une playlist
-    $SQL="INSERT INTO Playlist (nom, lien_image) VALUES ('$nom_playlist', '$lien_image')";
-    SQLInsert($sql);
-
-    return SQLGetChamp("SELECT id_playlist FROM Playlist WHERE nom='nom_playlist' && lien_image = 'lien_image';");
+    // A MODIFIER
+	
+    /*$SQL="INSERT INTO Playlist (nom, lien_image) VALUES ('$nom_playlist', '$lien_image')";
+    return SQLInsert($sql); */ 
 }
 
 function mkCommentaire ($idPlaylist, $idUser, $contenu)
 {
     // Créer un commentaire
 
-   $SQL="INSERT INTO Commentaire (id_playlist, id_user, commentaire) VALUES ('$idPlaylist','$idUser','$contenu';");
-    SQLInsert($sql);
-
-    return SQLGetChamp("SELECT commentaire FROM Commentaire WHERE id_playlist='$ifPlaylist' && id_user= '$idUser';");
-
+   $SQL="INSERT INTO Commentaire (id_playlist, id_user, commentaire) VALUES ('$idPlaylist','$idUser','$contenu';)";
+    return SQLInsert($SQL);
 }
 
 function aimerPlaylist($idPlaylist, $idUser)
@@ -47,22 +44,34 @@ function retirerAimer($idPlaylist, $idUser)
 function mkUser($pseudo, $mdp)
 {
     // Cette fonction crée un nouvel utilisateur et renvoie l'identifiant de l'utilisateur créé
+    $sql="INSERT TABLE user(`pseudo`,`password`,`admin`)
+    VALUES ($pseudo,$mdp,0);";
+    return SQLInsert($sql);
 }
 
-
-function changerMDP($pseudo, $new_mdp, $mdp)
+function changerMDP($pseudo, $new_mdp,$mdp)
 {
     // Changer le mot de passe de l'utilisateur
+    $sql="UPDATE user
+    SET `password`=$new_mdp
+    WHERE `pseudo`=$pseudo AND `password`=$mdp;";
+    return SQLUpdate($sql);
 }
-
 function changerPseudo($new_pseudo, $pseudo, $mdp)
 {
     // Changer le pseudo
+    $sql="UPDATE user
+    SET `pseudo`=$new_pseudo
+    WHERE `pseudo`=$pseudo AND `password`=$mdp;";
+    return SQLUpdate($sql);
 }
-
 function isAdmin($idUser)
 {
     // Vérifie si l'utilisateur est un administrateur
+    $sql="SELECT *
+    FROM user
+    WHERE `admin`=1 AND $idUser=`user`;"; 
+    return SQLSelect($sql);
 }
 
 function verifUserBdd($login,$passe)
